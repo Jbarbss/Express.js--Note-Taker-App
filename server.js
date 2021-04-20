3282b15790ca56e06191c76c65f313a8c1a52faa
Jbarbss <jbarbanel@gmail.com>
2021-04-20 12:10:21 -0400

    finished the server.js file

--- server.js
+++ server.js
@@ -4,5 +4,14 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require ("./routes/routes")(app);
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
