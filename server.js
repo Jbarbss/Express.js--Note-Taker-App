9fe427bda4eaa6cd5385d32a13105523a77a32b4
Jbarbss <jbarbanel@gmail.com>
2021-04-20 11:10:47 -0400

    added server.js file

---
+++ server.js
@@ -0,0 +1,8 @@
+//Dependencies required
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
