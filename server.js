d1b752e4bd67c90b87ee52c4d063d13ef08c3c5d
Jbarbss <jbarbanel@gmail.com>
2021-04-23 11:23:27 -0400

    updated route js files

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(__dirname + "/public"));
 
-// require ("./routes/apiRoutes")(app);
+app.use(express.static(__dirname + "/public"));
+
+
+require ("./routes/apiRoutes")(app);
 require ("./routes/htmlRoutes")(app);
 
 
